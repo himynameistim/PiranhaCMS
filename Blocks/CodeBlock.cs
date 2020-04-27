eec6c86ca79b66afad03175f2ee9c7aaeb2222df
timgriff84 <5212530+timgriff84@users.noreply.github.com>
2020-04-27 13:42:00 +0100

    Initial commit

---
+++ Blocks/CodeBlock.cs
@@ -0,0 +1,17 @@
+﻿using Piranha.Extend;
+using Piranha.Extend.Fields;
+
+namespace PiranhaCMS3.Blocks
+{
+    [BlockType(Name = "Code", Category = "Content",
+        Icon = "fas fa-code", Component = "code-block")]
+    public class CodeBlock : Block
+    {
+        /// <summary>
+        /// Gets/sets the text body.
+        /// </summary>
+        public TextField Body { get; set; }
+
+        public StringField Language { get; set; }
+    }
+}
---
+++ wwwroot/assets/js/Code-block.js
@@ -0,0 +1,22 @@
+﻿/*global
+    piranha
+*/
+
+Vue.component("code-block", {
+    props: ["uid", "toolbar", "model"],
+    methods: {
+        onBlur: function (e) {
+            this.model.body.value = e.target.innerText;
+        }
+    },
+    computed: {
+        isEmpty: function () {
+            return piranha.utils.isEmptyText(this.model.body.value);
+        }
+    },
+    template:
+        "<div class='block-body' :class='{ empty: isEmpty }'>" +
+        " <select v-model='model.language.value' class='form-control'> <option value='csharp'>C#</option><option value='xml'>XML</option></select>" +
+        "  <pre contenteditable='true' spellcheck='false' v-html='model.body.value' v-on:blur='onBlur'></pre>" +
+        "</div>"
+});
